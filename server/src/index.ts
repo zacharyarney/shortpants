647bc5f997e145a3d2b556448d72872b3514afe1
zacharyarney <zachary.arney@gmail.com>
2020-08-03 16:29:20 -0500

    adds MongoClient connection to server/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,31 @@
+require('dotenv').config();
 import server from './api/server';
+import { MongoClient } from 'mongodb';
 
 const port = 5000;
+let mongodbUrl: string;
 
-server.listen(port, () => {
-  console.log('\n=== SERVER LISTENING ON 5000 ===\n');
-});
+// dotenv uses `T | undefined` for it's environment variables, which breaks
+// the MongoClient connect method because it expects only a string.
+// This conditional forces mongodbUrl to be a string instead of string | undefined
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
+} else {
+  mongodbUrl = 'url not found';
+}
+
+MongoClient.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(client => {
+    // DB injection goes here, i.e.
+    // someModelDAO.injectDb(client);
+    server.listen(port, () => {
+      console.log('\n=== SERVER LISTENING ON 5000 ===\n');
+    });
+  })
+  .catch(e => {
+    console.error(e.stack);
+    process.exit(1);
+  });
