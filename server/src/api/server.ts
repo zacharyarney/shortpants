0f940ae3fffadabeb12646364616312fa4e778ba
zacharyarney <zachary.arney@gmail.com>
2020-09-28 17:40:22 -0500

    adds routes for addUrl and retrieveUrl to server.ts

--- server/src/api/server.ts
+++ server/src/api/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Errback, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { addUrl, retrieveUrl } from './controllers/urlController';
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hey there!', who: 'you!', wow: 'wowee!' });
 });
+
+// ROUTES
+app.post('/new', addUrl);
+app.get('/:hash', retrieveUrl);
 
 app.use((err: Errback, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ SERVER_ERROR: err });
