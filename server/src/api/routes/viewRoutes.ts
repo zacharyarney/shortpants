eaad47bae232b8ce605bcc44da30f9cfc73209b9
zacharyarney <zachary.arney@gmail.com>
2020-10-05 19:14:24 -0500

    adds route for submittedView

--- server/src/api/routes/viewRoutes.ts
+++ server/src/api/routes/viewRoutes.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import path from 'path';
+import submittedView from '../views/submittedView';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.sendFile(path.resolve(process.cwd(), 'src/api/static/submitUrl.html'));
+});
+
+router.get('/view/:hash', (req, res) => {
+  const view = submittedView(req.params.hash);
+  console.log(view);
+  res.set('Content-Type', 'text/html');
+  res.status(200).send(view);
 });
 
 export default router;
