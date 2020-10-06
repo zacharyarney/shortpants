55532439a0903fb5270992931888b3e288462e98
zacharyarney <zachary.arney@gmail.com>
2020-10-05 19:11:24 -0500

    adds submittedView template

---
+++ server/src/api/views/submittedView.ts
@@ -0,0 +1,20 @@
+function submittedView(hash: string) {
+  const domain = 'localhost:5000/';
+  const template = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>SHORTPANTS</title>
+    </head>
+    <body>
+      <h1>SHORTPANTS</h1>
+      <h3>Here's your shortened URL!</h3>
+      <p>${domain}${hash}</p>
+    </body>
+    </html>`;
+
+    return template;
+}
+
+export default submittedView;
