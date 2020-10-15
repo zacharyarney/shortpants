95163dc1f94e0f1fcdf0e3231652ba5930e425a3
zacharyarney <zachary.arney@gmail.com>
2020-10-15 15:05:24 -0500

    links styling for submittedView

--- server/src/api/views/submittedView.ts
+++ server/src/api/views/submittedView.ts
@@ -5,6 +5,12 @@ function submittedView(hash: string) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link
+        rel="stylesheet"
+        href="/css/index.css"
+        media="screen"
+        charset="UTF-8"
+      />
       <title>SHORTPANTS</title>
     </head>
     <body>
