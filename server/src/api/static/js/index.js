c65ca02a468c0c41b9087d0acc46181df0797a0f
zacharyarney <zachary.arney@gmail.com>
2020-10-15 18:34:13 -0500

    changes form submit url to https

--- server/src/api/static/js/index.js
+++ server/src/api/static/js/index.js
@@ -17,8 +17,11 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   console.log('ANYTHING');
   console.log(formUrl.value);
-  submitForm('http://localhost:5000/api/new', { url: formUrl.value })
+  submitForm('https://localhost:5000/api/new', { url: formUrl.value })
     .then(res => res)
-    .then(data => console.log(data[0]))
+    .then(data => {
+      console.log(data);
+      window.location = data.url;
+    })
     .catch(e => console.log(e));
 });
