bbd41e5d2c17a987edba9f59befd7dec2e6b526d
zacharyarney <zachary.arney@gmail.com>
2020-10-16 19:05:37 -0500

    removes routes, consolidates endpoints

--- server/src/api/routes/urlRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addUrl, getUrl } from '../controllers/urlController';
-
-const router = express.Router();
-
-router.post('/new', addUrl);
-router.get('/:hash', getUrl);
-
-export default router;
